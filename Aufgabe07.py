bb9803ce711c8fc65a2cf298b09e42e58a6379d7
Bryan Hain <bryan.hain@micromerce.com>
2022-01-09 19:10:31 +0100

    Hausaufgabe 8 Python OOP

--- Aufgabe07.py
+++ Aufgabe07.py
@@ -1,79 +1,70 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 17 12:33:56 2021
-
-@author: seher
-"""
 import csv
-import subplot
+from subplot import create_subplot
 import matplotlib as plt
-
 
 
 x=[]
 y=[]
 
 
-while True :
-    dateiname=input("CSV Dateiname: ")
-    
+while True:
+    dateiname = input("CSV-Datei einlesen: ")
     try:
-        with open(dateiname,"r",encoding="utf-8") as datei:
-            reader=csv.reader(datei,delimiter=";")
-            for row in reader:
-                x.append(float(row[0]))
-                y.append(float(row[1])) 
-           
+        with open(dateiname, "r", encoding="utf-8") as datei:
+            zeilen = csv.reader(datei, delimiter=";")
+            for zeile in zeilen:
+                x.append(float(zeile[0]))
+                y.append(float(zeile[1]))
         break
     except FileNotFoundError:
-        print("Datei wurde nicht gefunden. Bitte erneut einen Dateinamen eingeben.")
+        print("Datei wurde nicht gefunden, bitte erneut eingeben.")
+# DATEI.CSV EINLESEN
 
-       
-while True :
-    skalierung=(input("Skalierungsfaktor: "))
-    
+
+
+
+while True:
     try:
-        float(skalierung)
+        skalierung = float(input("Skalierungsfaktor eingeben: "))
         break
     except ValueError:
-        print("Der Skalierungsfaktor muss vom Typ Float sein.")
-
-while True:
-    startprozent=(input("Start Prozent: "))
-
-    
-    try:
-         float(startprozent)
-         if 0 <= float(startprozent) <=100:
-             break
-         else:
-             print("Es werden Floats zwischen 0-100 erwartet.")
-             
-
-    except ValueError:
-        print("Es werden Floats als Eingabe erwartet.")
+        print("Skalierungsfaktor muss vom Typ Float sein!")
+# SKALIERUNGSFAKTOR EINLESEN
 
 
-            
-    
-while True:
-    endprozent=(input("End Prozent: "))
 
-    
-    try:
-         float(endprozent)
-         if 0 <= float(endprozent) <=100:
-             break
-         else:
-           print("Es werden Floats zwischen 0-100 erwartet.")
-     
-    except ValueError:
-        print("Es werden Floats als Eingabe erwartet.")
-
-
-for wert in x:
-    wert = wert * skalierung
+y_scaled = []
 for wert in y:
-    wert = wert * skalierung
-# MODIFY all array
-subplot.createsubplot(x,y,startprozent,endprozent)
+    y_scaled.append(wert * skalierung)
+print(y_scaled)
+# SKALIERUNG DER Y LISTE
+
+
+
+
+while True:
+    try:
+        startProzent = float(input("Bitte Startprozent eingeben: "))
+        if(startProzent >= 0 and startProzent <=100):
+            break
+        else:
+            print("Startprozent sollte innerhalb (0-100) liegen!")
+    except ValueError:
+        print("Startprozent muss vom Typ Float sein!")
+# STARTPROZENT ABFRAGEN
+
+
+
+
+while True:
+    try:
+        endProzent = float(input("Bitte Endprozent eingeben: "))
+        if(endProzent >= startProzent and endProzent > 0 and endProzent <= 100):
+            break
+        print("Endprozent muss größer als Startprozent sein und innerhalb (0-100) sein!")
+    except ValueError:
+        print("Endprozent muss vom Typ Float sein!")
+# ENDPROZENT ABFRAGEN
+        
+
+create_subplot(x,y_scaled,startProzent,endProzent)
--- subplot.py
+++ subplot.py
@@ -6,45 +6,18 @@ Created on Sat Dec 18 16:36:12 2021
 """
 
 
+import matplotlib as plt
+import math
 
-import matplotlib.pyplot as plt
-
-
-
-def createsubplot(x,y,startprozent,endprozent):
-
-       startprozent=float
-       endprozent=float
-       z=len(x)
-       
-       
-       plt.plot(x,y,label="Graph",)
-       plt.xlabel=("x in π")
-       plt.ylabel=("y")
-       plt.xlim((z*startprozent)/100, (z*endprozent)/100)
-
-       plt.title=("Plot")
-       plt.legend()
-       plt.savefig("plot.pdf", format="pdf")
-       plt.show()
-
-
-def create_subplot(x, y, startprozent, endprozent):
-       # startprozent, endprozent sind Floats und x,y sind Listen
-       # 1. Anzahl der Elemente von X und Y bestimmen; vorausgesetzt: len(x) == len(y)
-       länge = len(x)
-       länge_prozent = länge / 100                             #länge von x,y = 500; 1% == 5 elemente
-
-       # 2. X,Y - Werte sind skaliert, aber Bereich muss noch ausgeschnitten werden
-       # Da Liste[2.3] nicht möglich => runden
-
-       # annahme: übergebenes X hat 120 elemente:
-       anfang = round(startprozent, 0) * länge_prozent         # startprozent = 23.5% ==> 23 * 5 elemente 
-       ende = round(ende, 0) * länge_prozent                   # endprozent = 84.6% ==> 85 * 5 elemente 
-       plt.pyplot(x[anfang:ende], y[anfang:ende])
-       plt.xlabel("x-Achse, skaliert")
-       plt.ylabel("y-Achse, skaliert")
-       plt.title("Graph")
-       plt.legend()
-       plt.savefig("plot.pdf", format="pdf")
-       plt.show()
+def create_subplot(x,y,start,end):
+    
+    #Ausschnitt start-end berechnen
+    len_xy_percent = math.floor(len(x)/100)
+    start = math.floor(start)*round(len_xy_percent,0)
+    end = math.ceil(end)*round(len_xy_percent,0)
+    plot_x = x[start:end]
+    plot_y = y[start:end]
+    print(plot_x, plot_y) # gibt  X-bereich und zugehörigen Y-Werte aus, len(x) == len(y)
+    
+    # Graph bauen aus plot_x und plot_y
+    # Achsenbeschriftung mit plot_x, plot_y
